086c6a311f572ef835b038755bfc603dcff71bb3
ivan <spishniak@gmail.com>
2022-02-01 01:34:19 +0200

    first step finish

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+app.get('/test', (req, res) => {
+  res.json({
+    hello: 'word',
+  });
+});
+
 app.listen(3000, () => {
   console.log('app listen on http://localhost:3000');
 });
